05cbbf95e957f1de07287e2a16555eb299ccbe72
João Pedro Teles de Andrades Pereira <joao.andrades@dtidigital.com.br>
2020-04-04 20:49:49 -0300

    .

---
+++ src/caixa/Caixa.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Caixa.css";
+
+const Caixa=()=>{
+    return (
+        <div className='container'>
+            <h1>Hello,<br/>I'm João</h1>
+            <h2>Meet more me in the buttons...</h2>
+            <button class="btn"><i class="fa fa-home"/>GitHub</button>
+            <button class="btn"><i class="fa-home"/>LinkedIn</button>
+        </div>
+    )
+}
+
+export default Caixa;
