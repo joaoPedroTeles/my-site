510d6ac259ad184c7cbb98d8fe1eeae038cc78f1
João Pedro Teles de Andrades Pereira <joao.andrades@dtidigital.com.br>
2020-04-04 22:16:24 -0300

    Updates

---
+++ precache-manifest.c8a692c50eaab8d0ec1319e4b5a42584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45037a7ff9fc704884bcccf09c8f2ce",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "a11bc051b084c618bdb6",
+    "url": "/my-site/static/css/main.943ffff5.chunk.css"
+  },
+  {
+    "revision": "027f3a91f8f22f107f53",
+    "url": "/my-site/static/js/2.baaa5cde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-site/static/js/2.baaa5cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11bc051b084c618bdb6",
+    "url": "/my-site/static/js/main.3960a568.chunk.js"
+  },
+  {
+    "revision": "621e7c932ea738ae0aeb",
+    "url": "/my-site/static/js/runtime-main.d8b2d259.js"
+  }
+]);
