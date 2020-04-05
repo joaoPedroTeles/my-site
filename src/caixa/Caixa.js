82eb6d09fdac4855f4c6f02bdd65083e42396662
João Pedro Teles de Andrades Pereira <joao.andrades@dtidigital.com.br>
2020-04-04 22:24:50 -0300

    .

--- src/caixa/Caixa.js
+++ src/caixa/Caixa.js
@@ -5,7 +5,7 @@ const Caixa=()=>{
     return (
         <div className='container'>
             <h1>Hello,<br/>I'm João</h1>
-            <h2>Meet more me in the buttons...</h2>
+            <h2>Conheça meus projetos e um pouco mais sobre mim nos botões</h2>
             <a class="btn" href="https://github.com/seteljota">GitHub</a>
             <a class="btn"href="https://www.linkedin.com/in/joaopedroteles">LinkedIn</a>
         </div>
