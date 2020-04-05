5709a7ee5902bfc5f1283dc0f1ea1391f9dbb820
João Pedro Teles de Andrades Pereira <joao.andrades@dtidigital.com.br>
2020-04-04 21:50:02 -0300

    .

--- src/caixa/Caixa.js
+++ src/caixa/Caixa.js
@@ -6,8 +6,8 @@ const Caixa=()=>{
         <div className='container'>
             <h1>Hello,<br/>I'm João</h1>
             <h2>Meet more me in the buttons...</h2>
-            <button class="btn"><i class="fa fa-home"/>GitHub</button>
-            <button class="btn"><i class="fa-home"/>LinkedIn</button>
+            <a class="btn" href="https://github.com/seteljota">GitHub</a>
+            <a class="btn"href="https://www.linkedin.com/in/joaopedroteles">LinkedIn</a>
         </div>
     )
 }
